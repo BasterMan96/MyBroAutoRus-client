a675bec034c60df37d8fd95f44b1c891096fcd41
ZavarzinFront <bb117478@gmail.com>
2025-11-19 11:12:32 +0500

    add new page reviews

--- app/reviews/page.tsx
+++ app/reviews/page.tsx
@@ -1,4 +1,24 @@
 import React from "react";
+import Header from "../components/Header";
+import Breadcrumbs from "../components/Breadcrumbs";
+import Footer from "../components/footer";
 
+const SelectAutoPage: React.FC = () => {
+  const breadcrumbsItems = [
+    { label: "Главная", href: "/" },
+    { label: "Отзывы" },
+  ];
 
+  return (
+    <>
+      <Header />
+      <Breadcrumbs items={breadcrumbsItems} />
+      <main className="">
+        <h1 className="">Отзывы</h1>
+      </main>
+      <Footer />
+    </>
+  );
+};
 
+export default SelectAutoPage;
